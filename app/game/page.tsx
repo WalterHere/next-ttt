b97724094e0af434f19bc916ccb35e2d84d9518b
BlueOliverLova <blueoliverlova@gmail.com>
2023-11-09 09:23:04 +0100

    added: box check method

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -14,6 +14,7 @@ export default function Page() {
     [2, 2, 2],
   ]);
   const [room, setRoom] = useState<string>("");
+  const [gameStatus, setGameStatus] = useState<number>(0);
   const [status, setStatus] = useState<number>(0);
   const [yourStatus, setYourStatus] = useState<number>(-1);
 
@@ -28,16 +29,15 @@ export default function Page() {
           socket.emit("login", localStorage.getItem("username"));
         });
 
-        socket.on("startGame", (...arg) => {
-          console.log(arg);
-          // setRoom(room);
-          // alert("Game started!");
+        socket.on("onStart", ({ room, firstTurn }) => {
+          setGameStatus(1);
+          setRoom(room);
 
-          // if (localStorage.getItem("username") === firstTurn) {
-          //   setYourStatus(0);
-          // } else {
-          //   setYourStatus(1);
-          // }
+          if (localStorage.getItem("username") === firstTurn) {
+            setYourStatus(0);
+          } else {
+            setYourStatus(1);
+          }
         });
 
         socket.on(
@@ -45,16 +45,54 @@ export default function Page() {
           ({
             row,
             col,
-            status,
+            status: opStatus,
           }: {
             row: number;
             col: number;
             status: number;
           }) => {
-            boxVals[row][col] = status;
-            setBoxVals([...boxVals]);
+            setBoxVals((vals) => {
+              const result = [...vals];
+              result[row][col] = opStatus;
 
-            setStatus(yourStatus);
+              let tmp = result[row].reduce((prev, val) =>
+                prev === val ? prev : -1
+              );
+              if (tmp === 0 || tmp === 1) {
+                for (let i = 0; i < 3; i++) {
+                  result[row][i] = 3;
+                }
+              }
+              tmp = result
+                .map((row) => row[col])
+                .reduce((prev, val) => (prev === val ? prev : -1));
+              if (tmp === 0 || tmp === 1) {
+                for (let i = 0; i < 3; i++) {
+                  result[i][col] = 3;
+                }
+              }
+              tmp = result
+                .map((row, index) => row[index])
+                .reduce((prev, val) => (prev === val ? prev : -1));
+              if (tmp === 0 || tmp === 1) {
+                for (let i = 0; i < 3; i++) {
+                  result[i][i] = 3;
+                }
+              }
+              tmp = result
+                .map((row, index) => row[2 - index])
+                .reduce((prev, val) => (prev === val ? prev : -1));
+              if (tmp === 0 || tmp === 1) {
+                for (let i = 0; i < 3; i++) {
+                  result[i][2 - i] = 3;
+                  console.log([...result]);
+                }
+              }
+
+              return result;
+            });
+
+            setStatus((opStatus + 1) % 2);
           }
         );
 
@@ -70,16 +108,13 @@ export default function Page() {
   }, []);
 
   const handleBoxToogle = (row: number, col: number) => {
-    if (status === yourStatus) {
-      boxVals[row][col] = status;
-      setBoxVals([...boxVals]);
-
+    if (status === yourStatus && boxVals[row][col] === 2) {
       socket.emit("draw", { row, col, status, room });
     }
   };
 
   return (
-    <main className="flex min-h-screen p-6">
+    <main className="md:flex min-h-screen p-6">
       <div className="flex flex-col">
         {boxVals.map((row, index) => (
           <div className="flex" key={index}>
@@ -92,6 +127,16 @@ export default function Page() {
             ))}
           </div>
         ))}
+      </div>
+
+      <div>
+        {gameStatus === 0 ? (
+          <span>Waiting for another user...</span>
+        ) : gameStatus === 1 ? (
+          <span>Game started</span>
+        ) : (
+          <span>Game Over</span>
+        )}
       </div>
     </main>
   );
